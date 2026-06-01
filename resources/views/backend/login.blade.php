a3a06c685de904fbbd9f51f31a73714e8ee27dc1
webwinnertechsolution-cmyk <webwinnertechsolution@gmail.com>
2026-06-01 11:53:23 +0530

    Update login.blade.php

--- resources/views/backend/login.blade.php
+++ resources/views/backend/login.blade.php
@@ -343,7 +343,7 @@
  
         <!-- Logo -->
         <div class="logo-wrap">
-            <img src="{{ asset('public/backend/assets/images/logo2.webp') }}"
+            <img src="{{ asset('backend/assets/images/logo-mini.svg') }}"
                  alt="SainiVet Pharma">
         </div>
  
