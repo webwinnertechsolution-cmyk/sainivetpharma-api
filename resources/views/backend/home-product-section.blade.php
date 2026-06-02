96079d92cd6dcf8f2f479c6d635752baad05ac45
webwinnertechsolution-cmyk <webwinnertechsolution@gmail.com>
2026-06-02 18:41:54 +0530

    Update home-product-section.blade.php

--- resources/views/backend/home-product-section.blade.php
+++ resources/views/backend/home-product-section.blade.php
@@ -156,7 +156,19 @@
 
     .form-check.form-switch { margin-left: 4px; margin-bottom: 12px; }
     .form-group label { font-size: 11px; line-height: 1; vertical-align: top; margin-bottom: 0.5rem; }
-
+    .form-check.form-switch {
+    width: 61%;
+    margin-left: 42px!important;
+}
+    .form-check .form-check-label {
+    display: block;
+    margin-left: 0;
+    font-size: 0.875rem;
+    line-height: 1.5;
+}
+    .btn.btn-sm, .ajax-upload-dragdrop .btn-sm.ajax-file-upload, .btn-group-sm > .btn, .ajax-upload-dragdrop .btn-group-sm > .ajax-file-upload {
+    font-size: 9px!important;
+}
     @media (max-width: 1024px) { .two-col { grid-template-columns: 1fr; } }
     @media (max-width: 768px) {
         .btn-group-custom { flex-direction: column-reverse; }
