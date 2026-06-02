c44ad0c9573294e8141a5319decd20587bae61bd
webwinnertechsolution-cmyk <webwinnertechsolution@gmail.com>
2026-06-02 19:20:15 +0530

    Update brand-section.blade.php

--- resources/views/backend/brand-section.blade.php
+++ resources/views/backend/brand-section.blade.php
@@ -162,7 +162,19 @@
     .modal-box-footer { padding: 10px 16px; display: flex; gap: 8px; justify-content: center; border-top: 1px solid #f3f4f6; }
 
     .existing-logo { margin-top: 8px; padding: 6px 8px; background: #f0f4f8; border-radius: 6px; display: inline-block; }
-
+ .form-check.form-switch {
+    width: 61%;
+    margin-left: 42px!important;
+}
+    .form-check .form-check-label {
+    display: block;
+    margin-left: 0;
+    font-size: 0.875rem;
+    line-height: 1.5;
+}
+    .btn.btn-sm, .ajax-upload-dragdrop .btn-sm.ajax-file-upload, .btn-group-sm > .btn, .ajax-upload-dragdrop .btn-group-sm > .ajax-file-upload {
+    font-size: 9px!important;
+}
     @media (max-width: 1024px) { .two-col { grid-template-columns: 1fr; } }
     @media (max-width: 768px) {
         .btn-group-custom { flex-direction: column-reverse; }
