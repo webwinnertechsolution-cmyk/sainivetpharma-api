304ca234a6f33786780ee4a8947ff6ece2479a84
webwinnertechsolution-cmyk <webwinnertechsolution@gmail.com>
2026-06-10 16:20:48 +0530

    Update slider.blade.php

--- resources/views/frontend/components/slider.blade.php
+++ resources/views/frontend/components/slider.blade.php
@@ -92,8 +92,9 @@
     background-color: green!important;
 	    padding-top: 10px !important;
 }
-
-
+.rl-slide-title {
+    color: #ffffff !important;
+}
 
 @media (max-width: 767px) {
 .rl-slider.slick-initialized.slick-slider {
