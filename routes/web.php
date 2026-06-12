add5b0e5141f54fab20e3076cc44111defbe2712
webwinnertechsolution-cmyk <webwinnertechsolution@gmail.com>
2026-06-12 17:32:28 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BackendController;
 use App\Http\Controllers\FrontendController;
 
+use App\Http\Controllers\InstagramController;
+
+Route::get('/instagram', [InstagramController::class, 'index']);
 // -----------------------------------------------------------------------------
 // Frontend Routes (New)
 // -----------------------------------------------------------------------------
