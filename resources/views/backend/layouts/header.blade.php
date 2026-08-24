ea7348f6893668d348c536fc15c0910bcdf825a7
webwinnertechsolution-cmyk <webwinnertechsolution@gmail.com>
2026-08-24 12:08:16 +0530

    Update header.blade.php

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -4,7 +4,7 @@
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
         <a class="navbar-brand brand-logo" href="{{ url('/dashboard') }}">
-            <img src="https://api.sainivetpharma.com/uploads/logo/1776854215_logo.png" alt="logo" /> 
+            <img src="https://api.sainivetpharma.com/uploads/logo/1781064546_logo.png" alt="logo" /> 
         </a>
         <a class="navbar-brand brand-logo-mini" href="{{ url('/dashboard') }}">
             <img src="https://api.sainivetpharma.com/uploads/logo/1776854215_logo.png" alt="logo" />
